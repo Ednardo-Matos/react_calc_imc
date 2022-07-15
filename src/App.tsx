51ea810c5cb701a53ddf2d8d57b8b42eb8aad49f
Ednardo-Matos <ednardoinacio@outlook.com>
2022-07-14 22:48:45 -0300

    desenvolvendo componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import powerImage from './assets/powered.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weightField, setWeightField] = useState<number>(0)
+
+  const hadleCalculateButton = () => {
+    if(heightField && weightField) {
+
+    }else {
+      alert("Digite todos os campos.")
+    }
+  }
+
+ return(
+  <div className={styles.main}>
+    <header>
+      <div className={styles.headerContainer}>
+        <img src={powerImage} alt="" width={150}/>
+      </div>
+    </header>
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <h1>Calcular o seu IMC.</h1>
+        <p>IMC é uma sigla para indice de massa Corporea.</p>
+
+        <input
+        type="number"
+        placeholder="Digite sua altura. Ex: 1.5 (em metros)"
+        value={heightField > 0 ? heightField : ''}
+        onChange={e => setHeightField(parseFloat(e.target.value))}
+        />
+
+        <input
+        type="number"
+        placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
+        value={weightField > 0 ? weightField : ''}
+        onChange={e => setWeightField(parseFloat(e.target.value))}
+        />
+
+        <button onClick={hadleCalculateButton}>Calcular</button>
+      </div>
+      <div className={styles.rightSide}>
+
+      </div>
     </div>
-  );
-}
+  </div>
 
+ );
+}
 export default App;
